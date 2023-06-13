1a89e645f8a13e650aeced69fe96ddd386bf62a5
HiteshAttri <hiteshattri71@gmail.com>
2023-06-13 20:10:53 +0530

    compilerApi2

--- methods/etherealCreds.js
+++ methods/etherealCreds.js
@@ -1,6 +1,6 @@
 let ethereal = {
-    etherealEmail : "tremaine.bayer@ethereal.email",
-    etherealPassword : "WdkUcrgfCr1XxjRKZv"
+    etherealEmail : "candace74@ethereal.email",
+    etherealPassword : "Yvq9CR5HWJhzxctNhz"
 }
 
 module.exports = ethereal;
--- routes/compiler.js
+++ routes/compiler.js
@@ -1,13 +1,10 @@
 const express=require("express")
-const fs = require("fs")
 const router=express.Router()
 app = express();
 const session = require("express-session")//
 
 const dir="e:\\study\\CLG\\Y3 S6\\DP";
 
-// const { Sender } = require("node-mailjet");
-// const sendEmail = require("../methods/sendEmail");
 app.use(session({
     secret: 'keyboard cat',//encoding
     resave: false,//for every request to    server even if req is from same user or browser it resetssession cookie 
@@ -16,13 +13,6 @@ app.use(session({
 }))
 
 router.get("/",(req, res) =>{
-    // if (req.session.login) {
-    //     res.redirect("/")
-    // }
-    // else {
-    //     res.render("login.ejs", { phrase: "" })
-    // }
-    // res.render("compiler")
     res.sendFile(dir+"/public/html/compiler.html");
 });
 
